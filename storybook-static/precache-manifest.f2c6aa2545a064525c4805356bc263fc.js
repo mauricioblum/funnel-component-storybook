02658ac4417d50043fbee617eadb60d5a597d4f2
Mauricio Blum <mauricio.blum@itsector.pt>
2020-12-16 12:29:48 -0300

    add build files

---
+++ storybook-static/precache-manifest.f2c6aa2545a064525c4805356bc263fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e62a4505b08a8cfc3bbfadb7037726",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.63a9bf73e44894212ccb.bundle.js"
+  },
+  {
+    "url": "runtime~main.63a9bf73e44894212ccb.bundle.js"
+  },
+  {
+    "revision": "349463b1a5189496f825",
+    "url": "static/css/main.16c2ec70.chunk.css"
+  },
+  {
+    "url": "vendors~main.63a9bf73e44894212ccb.bundle.js"
+  },
+  {
+    "url": "vendors~main.63a9bf73e44894212ccb.bundle.js.LICENSE.txt"
+  }
+]);
